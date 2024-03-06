0a3f4b5a081fae642b6be66969d3b4a7278611ae
Khairat Adesina <khairatadesina01@gmail.com>
2024-03-06 01:58:59 +0100

    add: sloved task 1 Block Scope

---
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable prefer-const */
+export default function taskBlock(trueOrFalse) {
+  let task = false;
+  let task2 = true;
+
+  if (trueOrFalse) {
+    let task = true;
+    let task2 = false;
+  }
+
+  return [task, task2];
+}
