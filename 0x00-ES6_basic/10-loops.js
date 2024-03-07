8fddf0dc6822dd7a0c5dcbd78b02937e78f0bc94
Khairat Adesina <khairatadesina01@gmail.com>
2024-03-07 03:36:14 +0100

    add: For...of Loops

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,9 @@
+//  For...of Loops
+export default function appendToEachArrayValue(array, appendString) {
+  for (const idx of array) {
+    const value = array.indexOf(idx);
+    array[value] = appendString + idx;
+  }
+
+  return array;
+}
