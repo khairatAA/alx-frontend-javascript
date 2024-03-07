eb0ab286d7014ea48518dd866038cdf0d26c6c76
Khairat Adesina <khairatadesina01@gmail.com>
2024-03-07 23:58:13 +0100

    fix: added retuen statement

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,13 +1,17 @@
 // Using the prototype below, return a promise. The parameter is a boolean.
 const handleResponseFromAPI = (promise) => new Promise((resolve, reject) => {
-  if (promise) {
-    console.log('Got a response from the API');
-    return resolve({
-      status: 200,
-      body: 'Success',
-    });
+  try {
+    if (promise) {
+      console.log('Got a response from the API');
+      return resolve({
+        status: 200,
+        body: 'Success',
+      });
+    }
+    throw new Error();
+  } catch (err) {
+    return reject(err);
   }
-  return reject(Error());
 });
 
 export default handleResponseFromAPI;
