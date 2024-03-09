423276eddde10313c2a4e0c8136da6e708cac90c
Khairat Adesina <khairatadesina01@gmail.com>
2024-03-09 13:49:02 +0100

    Throw an error

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -5,7 +5,7 @@ export default function guardrail(mathFunction) {
     const res = mathFunction();
     queue.push(res);
   } catch (error) {
-    queue.push(error.message);
+    queue.push(`${error.name}: ${error.message}`);
   } finally {
     queue.push('Guardrail was processed');
   }
