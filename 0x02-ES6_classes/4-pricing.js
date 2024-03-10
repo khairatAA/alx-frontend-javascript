70b987f0fe4d485e23accdcd125c4533167edf4e
Khairat Adesina <khairatadesina01@gmail.com>
2024-03-10 11:23:43 +0100

    add: Pricing

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,61 +1,61 @@
-import Currency from "./3-currency";
+import Currency from './3-currency';
 
 // Pricing
 
 export default class Pricing {
-    // Constructor
-    constructor(amount, currency) {
-        if (typeof (amount) !== 'number') {
-            return TypeError('Amount must be a number')
-        }
-        this._amount = amount
+  // Constructor
+  constructor(amount, currency) {
+    if (typeof (amount) !== 'number') {
+      TypeError('Amount must be a number');
+    }
+    this._amount = amount;
 
-        if (typeof (currency) !== 'object') {
-            return TypeError('Currency must be a object')
-        }
-        this._currency = currency
+    if (!(currency instanceof Currency)) {
+      TypeError('Currency must be an instance of Currency');
+    }
+    this._currency = currency;
+  }
+
+  // Getter and Setter for amount
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(amount) {
+    if (typeof (amount) !== 'number') {
+      TypeError('Amount must be a number');
+    }
+    this._amount = amount;
+  }
+
+  // Getter and Setter for currency
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(currency) {
+    if (!(currency instanceof Currency)) {
+      TypeError('currency must be an instance of Currency');
+    }
+    this._currency = currency;
+  }
+
+  // Display full price method
+  displayFullPrice() {
+    // const cur = new Currency(this._currency._code, this._currency._name);
+    return `${this._amount} ${this.currency.displayFullCurrency()}`;
+  }
+
+  // Display convert price Static mrthod
+  static convertPrice(amount, conversionRate) {
+    if (typeof (amount) !== 'number') {
+      return TypeError('Amount must be a number');
     }
 
-    // Getter and Setter for amount
-    get amount() {
-        return this._amount
+    if (typeof (conversionRate) !== 'number') {
+      return TypeError('Conversion Rate must be a number');
     }
 
-    set amount(amount) {
-        if (typeof (amount) !== 'number') {
-            return TypeError('Amount must be a number')
-        }
-        this._amount = amount
-    }
-
-    // Getter and Setter for currency
-    get currency() {
-        return this._currency
-    }
-
-    set currency(currency) {
-        if (typeof (currency) !== 'object') {
-            return TypeError('currency must be an object')
-        }
-        this._currency = currency
-    }
-
-    // Display full price method
-    displayFullPrice() {
-        const cur = new Currency(this._currency._code, this._currency._name)
-        return `${this._amount} ${cur.displayFullCurrency()}`
-    }
-
-    // Display convert price Static mrthod
-    static convertPrice(amount, conversionRate) {
-        if (typeof (amount) !== 'number') {
-            return TypeError('Amount must be a number')
-        }
-
-        if (typeof (conversionRate) !== 'number') {
-            return TypeError('Conversion Rate must be a number')
-        }
-
-        return amount * conversionRate
-    }
+    return amount * conversionRate;
+  }
 }
