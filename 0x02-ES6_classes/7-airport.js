538309139cb43a1d4d5165bfb3a411ef0bb7d7e2
Khairat Adesina <khairatadesina01@gmail.com>
2024-03-10 13:27:59 +0100

    add : Airport

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -1,20 +1,19 @@
 // Airport
 
 export default class Airport {
-    constructor(name, code) {
-        if (typeof (name) !== 'string') {
-            throw new TypeError('Name must be a string')
-        }
-        this._name = name
-
-        if (typeof code !== 'string') {
-            throw new TypeError('Code must be a string')
-        }
-        this._code = code
+  constructor(name, code) {
+    if (typeof (name) !== 'string') {
+      throw new TypeError('Name must be a string');
     }
+    this._name = name;
 
-    toString() {
-        return `Airport [${this._code}]`
+    if (typeof code !== 'string') {
+      throw new TypeError('Code must be a string');
     }
+    this._code = code;
+  }
 
+  toString() {
+    return `Airport [${this._code}]`;
+  }
 }
