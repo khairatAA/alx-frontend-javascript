59171f990de25e26d854f4d9b178a9497ed06ce0
Khairat Adesina <khairatadesina01@gmail.com>
2024-03-10 07:06:23 +0100

    add: implemented getter and setters

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,41 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    if (typeof value !== 'string') {
+      Error('Name must be a string');
+    }
+
+    this._name = value;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(value) {
+    if (typeof value !== 'number') {
+      throw Error('Length must be a number');
+    }
+    this._length = value;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(value) {
+    if (!Array.isArray(value)) {
+      Error('Student must be an array');
+    }
+    this._students = value;
+  }
+}
