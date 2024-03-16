8a3e7828aea80e7588830cc77e236a41bdfbb7be
Khairat Adesina <khairatadesina01@gmail.com>
2024-03-16 10:27:14 +0100

    add: Basic list of objects

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,25 @@
+// Basic list of objects
+
+export default function getListStudents() {
+  const arr = [];
+
+  arr[0] = {
+    id: 1,
+    firstName: 'Guillaume',
+    location: 'San Francisco',
+  };
+
+  arr[1] = {
+    id: 2,
+    firstName: 'James',
+    location: 'Columbia',
+  };
+
+  arr[2] = {
+    id: 5,
+    firstName: 'Serena',
+    location: 'San Francisco',
+  };
+
+  return arr;
+}
