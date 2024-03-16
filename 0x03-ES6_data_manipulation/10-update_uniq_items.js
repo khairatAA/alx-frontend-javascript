a81ee3a62ba15c4a6638b37bce7660cd3076f885
Khairat Adesina <khairatadesina01@gmail.com>
2024-03-16 15:17:23 +0100

    add: More map data structure

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,19 @@
+// More map data structure
+
+export default function updateUniqueItems(map) {
+  if (!(map instanceof Map)) {
+    throw new Error('Cannot process');
+  }
+
+  const updatedMap = new Map();
+
+  for (const [key, value] of map.entries()) {
+    if (value === 1) {
+      updatedMap.set(key, 100);
+    } else {
+      updatedMap.set(key, value);
+    }
+  }
+
+  return updatedMap;
+}
