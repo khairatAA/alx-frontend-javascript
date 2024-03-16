11dec5289d39f49baa50db3ff6ca85db76c8d87c
Khairat Adesina <khairatadesina01@gmail.com>
2024-03-16 14:20:40 +0100

    add:Map data structure

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -1,11 +1,11 @@
 // Map data structure
 
-export default function groceriesList(params) {
-    return new Map([
-        ['Apples', 10],
-        ['Tomatoes', 10],
-        ['Pasta', 1],
-        ['Rice', 1],
-        ['Banana', 5],
-    ])
+export default function groceriesList() {
+  return new Map([
+    ['Apples', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5],
+  ]);
 }
